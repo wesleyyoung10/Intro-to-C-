9b200b31440f69fa4bb62be02cb5b25313c22c53
Young <wyoung@proforma.com>
2018-04-03 19:15:27 -0400

    Intro to C#

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -10,6 +10,43 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
+            int dogsInPark = 5;
+
+            Console.WriteLine($"There are { dogsInPark } dogs in the park");
+
+            decimal sandwiches = 1.5M;
+
+            Console.WriteLine($"I can eat { sandwiches } sandwiches in one sitting");
+
+            decimal money = 60.0M;
+
+            Console.WriteLine($"A nice dinner costs ${ money } to buy");
+
+            string colorOfCar = "black";
+
+            Console.WriteLine($"My dream car color is { colorOfCar }");
+            
+            string makeOfCar = "Chevy";
+
+            Console.WriteLine($"The make of my dream car is { makeOfCar }");
+
+            string modelOfCar = "Silvardo";
+
+            Console.WriteLine($"The model of my dream car is { modelOfCar }");
+
+            DateTime today = DateTime.Now;
+
+            Console.WriteLine($"Todays date is { DateTime.Now }");
+
+            string letter = "W";
+
+            Console.WriteLine($"The first letter of my name is { letter }");
+
+            int stars = 20000000;
+
+            Console.WriteLine($"There are { stars } stars in the sky that we can see");
+
+            Console.Read();
         }
     }
 }
